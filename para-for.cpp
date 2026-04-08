dcc23bfc94ea9e9672ec4b963607c292a2feb000
SoWiEee <kawiisowieee@gmail.com>
2026-04-08 11:27:13 +0800

    Use omp timer

--- para-for.cpp
+++ para-for.cpp
@@ -1,14 +1,17 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 void printout(int n) {
     for(int i=0; i<10000; i++)
     { /* do nothing */ }
-    printf("%d, ",n);
+    printf("%d, ", n);
 }
 
 int main(int argc, char* argv[]) {
+    double start_time = omp_get_wtime();
     #pragma omp parallel for
-    for(int i=0; i<16; i++) printout(i);
-    return 0;
+        for(int i=0; i<12; i++) printout(i);
+    double end_time = omp_get_wtime();
+    printf("Real time: %f sec\n", end_time - start_time);
 }
